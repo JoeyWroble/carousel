9c7787ff655f3c68664783d60771028f73cd8050
JoeyWroble <jwroble@uiowa.edu>
2023-07-10 12:56:39 -0500

    Migrations

--- db/migrate/20230710171334_create_products.rb
+++ db/migrate/20230710171334_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :title
-      t.array :image_urls
+      t.string :image_urls
 
       t.timestamps
     end
