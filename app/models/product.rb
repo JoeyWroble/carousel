cccaf29535b1ecff9420a7a60f70d1765a26809d
JoeyWroble <jwroble@uiowa.edu>
2023-07-18 13:58:01 -0500

    Back functioning with images - need to center

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,5 @@
 class Product < ApplicationRecord
-    
-    def new_image_urls
-        
-    end
-
     def image_urls
-        image_urls = new_image_urls
         image_urls_text.split(',').map(&:strip)
     end
 end
