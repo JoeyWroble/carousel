aecc48dbc47f0bc31b0fe14a9b9305769233f64d
gary shields <gary@bomdias.com>
2023-07-20 12:31:19 -0500

    Added Dialog example.

---
+++ app/javascript/controllers/dialog_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static targets = [
+    "form", "dialog"
+  ];
+
+  connect() {
+    // The class we should toggle on the container
+    this.toggleClass = this.data.get('class') || 'hidden';
+
+  }
+
+  disconnect() {
+    this.close();
+  }
+
+  open( e ) {
+
+    this.dialogTarget.showModal();
+
+    this.formTarget.addEventListener( 'submit', event => {
+      this.close();
+    })
+
+  }
+
+  close( e ) {
+    this.dialogTarget.close();
+  }
+
+  click( e ) {
+
+    if( e.target === this.dialogTarget ) {
+      this.close(e);
+    }
+
+  }
+
+}
