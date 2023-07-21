a6a680e91fa747db4d53e371bb8e5e7bdd53937e
JoeyWroble <jwroble@uiowa.edu>
2023-07-21 14:32:12 -0500

    renames and cleanup

--- app/javascript/carousel.js
+++ app/javascript/carousel.js
@@ -1,18 +1,10 @@
-function myFunction(imgs) {
-    var expandImg2 = document.getElementById("expandedImg2");
-    var imgText2 = document.getElementById("imgtext2");
+function expandImg1(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
     
-    expandImg2.src = imgs.src;
-    imgText2.innerHTML = imgs.alt;
-    expandImg2.parentElement.style.display = "block";
-    
-  }
-  
-  function closeExpandedImage2() {
-    var expandImg2 = document.getElementById("expandedImg2");
-    
-    expandImg2.src = "";
-    expandImg2.parentElement.style.display = "none";
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
     
   }
   
