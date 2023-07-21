42a9c893fa5f28486d266f28d5695bd477b5c88b
JoeyWroble <jwroble@uiowa.edu>
2023-07-21 10:36:41 -0500

    both carousels working

--- app/javascript/carousel.js
+++ app/javascript/carousel.js
@@ -1,18 +1,18 @@
 function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
+    var expandImg2 = document.getElementById("expandedImg2");
+    var imgText2 = document.getElementById("imgtext2");
     
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
+    expandImg2.src = imgs.src;
+    imgText2.innerHTML = imgs.alt;
+    expandImg2.parentElement.style.display = "block";
     
   }
   
-  function closeExpandedImage() {
-    var expandImg = document.getElementById("expandedImg");
+  function closeExpandedImage2() {
+    var expandImg2 = document.getElementById("expandedImg2");
     
-    expandImg.src = "";
-    expandImg.parentElement.style.display = "none";
+    expandImg2.src = "";
+    expandImg2.parentElement.style.display = "none";
     
   }
   
