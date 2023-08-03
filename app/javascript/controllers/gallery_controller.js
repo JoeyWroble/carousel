83a808a36cdfa57525ac03eea7580524dfc20f4a
JoeyWroble <jwroble@uiowa.edu>
2023-08-03 12:20:40 -0500

    bad image check

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="gallery"
 export default class extends Controller {
   connect() {
+    this.checkImgUrls();
   }
 
   static targets = ["gallery"];
@@ -22,4 +23,20 @@ expandMod(event) {
   modalExpandImg.src = img.src;
 }
 
+checkImgUrls() {
+  const imgBtns = document.querySelectorAll(".column img");
+  imgBtns.forEach((img) => {
+    const imgElement = img;
+    const imageObj = new Image();
+    imageObj.onload = function () {
+      if (imageObj.naturalWidth == 0 && imageObj.naturalHeight == 0) {
+        imgElement.parentElement.style.display = "none";
+      }
+    };
+    imageObj.onerror = function () {
+      imgElement.parentElement.style.display = "none";
+    };
+    imageObj.src = img.src;
+  });
+};
 }
